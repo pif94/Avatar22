1fdf022baec698452d154c1e2bb21994b5310a36
Balan M Daria-Cristiana <131289324+dbqx@users.noreply.github.com>
2023-11-21 20:10:29 +0200

    Update Par.java

--- src/main/java/student/ugal/ro/avatar22/Par.java
+++ src/main/java/student/ugal/ro/avatar22/Par.java
@@ -1,62 +1,75 @@
 package student.ugal.ro.avatar22;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+
 /**
  *
  * @author db343
  */
-public class Par extends Avatar {
 
+package avatar;
+
+
+
+/**
+ *
+ * @author db343
+ */
+
+public class Par extends Avatar {
     private String culoarePar;
+    private int index;
+    private String numePng;
 
     // const fara arg
-    public Par()
-    {
-        super();
-        this.culoarePar = culoarePar;
+    public Par() {
+        super(); 
+        this.culoarePar = " ";
+        this.index = 0;
+        this.numePng = "Par_" + this.index + ".png"; 
     }
 
-    // const cu toate argumentele
-    public Par(int layer, int pozitieX, int pozitieY, String culoarePar)
-    {
-        super(layer, pozitieX, pozitieY);
+    // const cu toate arg
+    public Par(int layer,  int pozitieX, int pozitieY, String culoarePar, int index) {
+        super(layer, pozitieX, pozitieY); 
         this.culoarePar = culoarePar;
+        this.index = index;
+        this.numePng = "Par_" + this.index + "_" + this.culoarePar + ".png"; 
     }
 
     // const de copiere
-    public Par(Par p)
-    {
-       
-        this.culoarePar = p.culoarePar;
+    public Par(Par p) {
+        super(p); 
+        this.culoarePar = culoarePar;
+        this.index = p.index;
+        this.numePng = p.numePng;
+    }
+ 
+    public int getIndex() {
+        return index;
     }
 
-    // met 
-    public String getCuloarePar()
-    {
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getNumePng() {
+        return numePng;
+    }
+
+    public void setNumePng(String numePng) {
+        this.numePng = numePng;
+    }
+    public void getCuloarePar( ) {
         return culoarePar;
     }
-
-    public void setCuloarePar(String culoarePar)
-    {
-        this.culoarePar = culoarePar;
+    public void setCuloarePar(String culoarePar) { 
+        this.culoarePar=culoarePar;
+    }
+         
+    // met ToString rescrisa
+      @Override
+    public String toString() {
+        return "Par: layer = " + getLayer() + ",  pozitieX = " + getPozitieX() + ", pozitieY = " + getPozitieY() + ", culoarePar  = " + culoarePar + ", index = " + index + ", numePng = " + numePng;
     }
 
-    // met toString rescrisa
-    @Override
-
-    public String toString()
-    {
-        return "Par: layer = " + getLayer() + ", pozitieX = " + getPozitieX() + ", pozitieY = " + getPozitieY() + ", culoare par = " + getCuloarePar();
-    }
-
-    // alegere par din imaginile png
-    /* 
-        public void alegePar(String numeFisier) throws IOException {
-        File fisierPar = new File(numeFisier); 
-        BufferedImage par = ImageIO.read(fisierPar);
-    }
-     */
-}
+ }
